51a07e04bb4cd1df28edc3c4030be64c26e8f9d7
Alfredo Garcia <alfredo@fuzzyflags.com>
2024-11-15 18:02:12 -0600

    Fixed rainbow

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -7,29 +7,41 @@ import { create, StoreApi, UseBoundStore } from 'zustand'
 
 const rainbow = [
   {
-    color: '#FF5733',
-    alpha: (alpha: number) => `rgba(#FF5733, ${alpha})`
+    strong: 'rgb(91, 89, 190)',//purple
+    hex: '#5B59BE',
+    alpha: (v: number) => `rgba(91, 89, 190, ${v})`,
+    light: '#eeeef9',
   },
   {
-    color: '#33FF57',
-    alpha: (alpha: number) => `rgba(#33FF57, ${alpha})`
+    strong: 'rgb(191, 90, 90)',//red
+    hex: '#BF5A5A',
+    alpha: (v: number) => `rgba(191, 90, 90, ${v})`,
+    light: '#f8eeee',
   },
   {
-    color: '#3357FF',
-    alpha: (alpha: number) => `rgba(#3357FF, ${alpha})`
+    strong: 'rgb(149, 166, 70)',//orbit green
+    hex: ' #95A646',
+    alpha: (v: number) => `rgba(149, 166, 70, ${v})`,
+    light: '#f5f6ed',
   },
   {
-    color: '#FF33A1',
-    alpha: (alpha: number) => `rgba(#FF33A1, ${alpha})`
+    strong: 'rgb(89, 173, 190)',//blue
+    hex: '#59ADBE',
+    alpha: (v: number) => `rgba(89, 173, 190, ${v})`,
+    light: '#e0edef',
   },
   {
-    color: '#A133FF',
-    alpha: (alpha: number) => `rgba(#A133FF, ${alpha})`
+    strong: 'rgb(255, 145, 64)', // orange
+    hex: '#ff9140',
+    alpha: (v: number) => `rgba(255, 145, 64, ${v})`,
+    light: '#ffd5b8',
   },
   {
-    color: '#FFA133',
-    alpha: (alpha: number) => `rgba(#FFA133, ${alpha})`
-  }
+    strong: 'rgb(0, 167, 47)',//lemon green
+    hex: '#00A72F',
+    alpha: (v: number) => `rgba(0, 167, 47, ${v})`,
+    light: '#ebf6eb',
+  },
 ]
 
 type SortingField = {
