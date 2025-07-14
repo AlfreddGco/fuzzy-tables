4f571486cea558bbc3f38b8321ad3cb9e371753d
Alfredo Garcia <alfredo@fuzzyflags.com>
2025-07-14 17:21:12 -0600

    Fixed react not defined bug

--- src/components/Forms/Field/multiple-select.tsx
+++ src/components/Forms/Field/multiple-select.tsx
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { rainbow } from "../../../lib/utils";
 
 interface MultipleSelectProps {
